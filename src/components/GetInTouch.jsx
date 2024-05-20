c45428270c1e6cd27476d0ffa15387df76279fc0
Nath Mendoza <mendoza.nathaniel328@gmail.com>
2024-05-20 12:47:00 +1200

    form update

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -1,12 +1,43 @@
 import styled from "styled-components";
 import { NarrowDiv } from "../styles/ThemeContainers";
 import { TypeH2, TypeText } from "../styles/Typography";
+import { Input, TextArea, SubmitButton } from "../styles/Forms";
+
+import emailjs from '@emailjs/browser'
+import { useState } from "react";
+
 
 const GetInTouch = () => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleInputChange = e => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
+    const handleFormSubmit = e => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
     return (
         <NarrowDiv style={{marginBottom: '72px'}}>
             <TypeH2 style={{textAlign: 'center'}}>Get in Touch</TypeH2>
             <TypeText style={{textAlign: 'justify'}}>If you're interested in the work I do, feel free to reach out thru this form. Alternatively, you could also get in touch with my social accounts.</TypeText>
+            <form onSubmit={handleFormSubmit}>
+                <Input type="text" name="name" value={formData.name} onChange={handleInputChange} required placeholder="Name" />
+                <Input type="email" name="email" value={formData.email} onChange={handleInputChange} required placeholder="Email" />
+                <TextArea name="message" value={formData.message} onChange={handleInputChange} required placeholder="Message..." />
+                <SubmitButton type="submit">SUBMIT</SubmitButton>
+            </form>
         </NarrowDiv>
     )
 }
---
+++ src/styles/Forms.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { getTheme } from "./ThemeUtils";
+import { DefaultButton } from "./ThemeButtons";
+
+export const Input = styled.input`
+    width: 100%;
+    font-family: ${getTheme('primaryFont')};
+    font-size: 18px; 
+    line-height: 26px;
+    padding: 6px 12px;
+    color: ${getTheme('primaryColor')};
+    background-color: ${getTheme('lightColor')};
+    border-radius: 5px;
+    border: 1px solid #000;
+    margin: 0 0 16px;
+`
+
+export const TextArea = styled.textarea`
+    width: 100%;
+    font-family: ${getTheme('primaryFont')};
+    font-size: 18px; 
+    line-height: 26px;
+    padding: 6px 12px;
+    color: ${getTheme('primaryColor')};
+    background-color: ${getTheme('lightColor')};
+    border-radius: 5px;
+    height: 220px;
+    border: 1px solid #000;
+    margin: 0 0 16px;
+`
+
+export const SubmitButton = styled(DefaultButton)`
+    width: 180px;
+    border-color: ${getTheme('lightColor')};
+    color: ${getTheme('lightColor')};
+    &:hover {
+        background-color: ${getTheme('lightColor')};
+        color: ${getTheme('darkColor')};
+    }
+`
